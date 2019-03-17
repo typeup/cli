bc2cd373abd546d4f06148f968c76c37def08aad
Simon Mika <simon@mika.se>
2019-03-17 21:05:39 +0100

    Added first test.

---
+++ source/index.spec.ts
@@ -0,0 +1,8 @@
+import { Program } from "./"
+
+describe("command line", () => {
+	it("version", async () => {
+		const program = new Program(["version"])
+		expect(program.getVersion().slice(0, 4)).toEqual("0.2.")
+	})
+})
