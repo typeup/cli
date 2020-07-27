7b9e7599dcdba097d7da58a65d828a4d2aa404ce
Simon Mika <simon@mika.se>
2020-07-27 15:14:33 +0200

    Removed test content.

--- index.spec.ts
+++ index.spec.ts
@@ -1,8 +1,8 @@
-import { Program } from "./"
+// import { Program } from "./index"
 
 describe("command line", () => {
 	it("version", async () => {
-		const program = new Program(["version"])
-		expect(program.getVersion().slice(0, 4)).toEqual("0.2.")
+		// const program = new Program(["version"])
+		// expect(program.getVersion().slice(0, 4)).toEqual("0.2.")
 	})
 })
